a669f24f173397c03ff392ced9bfcf367941b9ec
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-02-28 17:45:09 +0900

    Intersection Observer added

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,7 @@ const options = {
   rootMargin: "100px 100px 100px 100px",
   threshold: 0,
 };
+
 // IntersectionObserver 를 등록한다.
 const io = new IntersectionObserver((entries, observer) => {
   entries.forEach((entry) => {
@@ -22,16 +23,20 @@ const io = new IntersectionObserver((entries, observer) => {
       // data-src 정보를 타켓의 src 속성에 설정
       entry.target.src = entry.target.dataset.src;
       // 이미지를 불러왔다면 타켓 엘리먼트에 대한 관찰을 멈춘다.
-      observer.unobserve(entry.target);
+      //   observer.unobserve(entry.target);
+    } else {
+      entry.target.src = "";
     }
   });
 }, options);
 
 function init() {
   defaultShowingImg();
-  images.forEach((target) => {
-    io.observe(target);
-  });
+  if ("IntersectionObserver" in window) {
+    images.forEach((target) => {
+      io.observe(target);
+    });
+  }
 }
 // 관찰할 대상을 선언하고, 해당 속성을 관찰시킨다.
 
